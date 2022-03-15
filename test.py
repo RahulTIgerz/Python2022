95e843bd35264b80e9ab52f69d7467c5539a25ee
RahulTIgerz <rahrao83@kingshigh.school.nz>
2022-03-16 12:50:57 +1300

    popopo

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-#print("Hello world")
+#Excercise 4.1
+print("Hello world")
 print("Rahul Rao Nilapu")
 print("Mrs White")
 print("Kings High School")
@@ -9,3 +10,9 @@ print("94")
 print("53")
 print("Tekken 7")
 print("Digital Technology")
+#Excercise 4.4
+print("We/'re no strangers to love")
+print("Frankly, my dear, I don/'t give a damn.")
+print(" My definiton of a string is collection of characters these can be letters and numbers.")
+print("An integer a whold number , this is numbers like 1,2,3.")
+print("A float is a number which is not fully a whole number such as 1.5.")
