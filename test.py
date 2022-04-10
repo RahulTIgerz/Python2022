9c937a3ce0487cc5cb93976972c486f35cc17505
RahulTIgerz <100237086+RahulTIgerz@users.noreply.github.com>
2022-04-10 17:44:42 +1200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-#Excercise 4.1
-'''print("Hello world")
+"""#Excercise 4.1 
+print("Hello world")
 print("Rahul Rao Nilapu")
 print("Mrs White")
 print("Kings High School")
@@ -10,6 +10,7 @@ print("94")
 print("53")
 print("Tekken 7")
 print("Digital Technology")
+
 #Excercise 4.4
 print("We/'re no strangers to love")
 print("Frankly, my dear, I don/'t give a damn.")
@@ -101,6 +102,7 @@ print("buying shoes")
 money = (money) - (shoes)
 print("You now have the following amount left over")
 print(money)
+
 #Excersice 6.3
 input("Enter hi, then press enter")
 input("Type your name, then press enter")
@@ -149,7 +151,7 @@ book = input("Can you give me a name of a book")
 print(book)
 num = int(input("Could i have a number"))
 str(print(book * num)) 
-'''
+
 #Exercises 7.5
 ice_cream = int(input("How many ice creams do you need"))
 if ice_cream > 0 and ice_cream <= 20:
@@ -177,3 +179,157 @@ if darth_plagueis_the_wise.lower() == "no":
 	print("Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
 else: print("You must be a fan")
 
+passion_christ = input("Who directed Passion of the Christ")
+if passion_christ.lower() == "mel gibson":
+	print("correct")
+else:
+	print("Mel Gibson did")	
+
+score = 0
+question_1 = int(input("What is 1 + 1?"))
+if question_1 == 2:
+	print("correct")
+	score += 1
+else:
+	print("incorrect")
+
+question_2 = input("Who is president of USA?")
+if question_2.lower() == ("joe biden"):
+	print("correct")
+	score += 1  
+else:
+	print("incorrect")
+
+question_3 = input("Who is prime minister of NZ?")
+if question_3.lower() == ("jacinda ardern"):
+	print("correct")
+	score += 1
+else:
+	print("incorrect")
+
+question_4 = int(input("What is 10 -5?"))
+if question_4 == 5:
+	print("correct")
+	score += 1
+else:
+	print("incorrect")
+
+question_5 = input("Did you enjoy the quiz?")		
+if question_5.lower() == ("yes"):
+	print("Bonus point")
+	score += 1
+else:
+	print("... point deduction")
+	score -= 1
+
+print(score)	
+
+
+#Excersise 8.7 
+fibonacci_sequence = [0,1,1,2,3,5,8,13,21,34]
+print(fibonacci_sequence)
+
+fav_fruits = ["mango","strawberry","water mellon", "grapes", "apples"]
+print(fav_fruits)
+
+fav_yt = ["Socksfor1","Socksfor2","Socksfor3","SocksReact","FatMemeGod" ]
+print(fav_yt)
+
+fav_songs = []
+fav_songs.append("Tabun")
+fav_songs.append("Yoru ni kakeru")
+fav_songs.append("Haruka")
+fav_songs.append("Ano Yume o Nazotte")
+fav_songs.append("Halzion")
+print(fav_songs)
+
+books = []
+book = input("What are your top 5 favourite books")
+books.append(book)
+books.sort()
+print(books)
+
+pizza_toppings = []
+pizza_toppings = input("Would you like some toppings such as Pepperoni, Ham, Pineapple, Cheese, Tomatoes, Spinnach")
+if pizza_toppings == ("Pepperoni"):
+	pizza_toppings.append("Pepperoni")
+if pizza_toppings == ("Ham"):
+	pizza_toppings.append(pizza_toppings)
+if pizza_toppings == ("Pineapple"):
+	pizza_toppings.append(pizza_toppings)
+if pizza_toppings == ("Cheese"):
+	pizza_toppings.append(pizza_toppings)
+if pizza_toppings == ("Tomatoes"):
+	pizza_toppings.append(pizza_toppings)
+if pizza_toppings == ("Spinnach"):
+	pizza_toppings.append(pizza_toppings)
+else:
+	print()
+print(pizza_toppings)
+
+fruits_list = ["Mango","Strawberry","Grape","Apple","Bannana"]
+fruit = input("Could you give me a name of a fruit")
+if fruit == ["Mango" or "Strawberry" or "Grape" or "Apple" or "Bannana"]:
+	print("Sorry im not after that fruit ")
+else:
+	fruits_list.append(fruit)
+print(fruits_list)		 
+
+name_list = ["Rahul","Gayatri","Prasanth","Uma","Babu"]
+name_list.sort()
+name_list.reverse()
+print(name_list)
+
+prime_numbers = [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+prime_numbers.reverse()
+print(prime_numbers)
+print("The lenght of this list is 25 numbers long ")
+
+comman_verbs = ["run"," jump","read","roll"]
+comman_verbs.sort()
+print(comman_verbs)
+
+
+"""#Exercise 9.2 
+print("Hi, welcome to Ice Crea, Maker")
+order_complete = False
+toppings_list = []
+
+toppings_available = ["vanila","strawberry","chocolate","sprinkles","nuts","raisens","chocolate sauce","flake","m&ms"]
+print("vanila\nstrawberry\nchocolate\nsprinkles\nnuts\nraisens\nchocolate sauce\nflake\nm&ms")
+topping = input("Please choose your topping from the list")
+if topping.lower() == toppings_available:
+	print("Sweet")
+else:
+	print("That is not an available topping")
+
+
+
+while order_complete == False:
+	topping = input("What topping? - push enter to finish")
+	if topping == "":
+		print("Order Done")
+		order_complete = True 
+	elif topping in toppings_list:
+		print("You already have that topping")
+	else:
+		print("Great, adding it to the list")
+print("Here are your toppings")
+
+topping_count = 0
+while topping_count <6:
+		topping_count += 1
+		
+
+
+"""#Excercise 9.3  
+increments = 0
+while increments <100:
+	increments += 2
+print(increments)	
+
+blast_off = 10
+while blast_off 10:
+	blast_off 
+
+"""	
