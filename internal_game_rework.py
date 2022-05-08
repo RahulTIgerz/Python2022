d48f4fa7dceb53b62727982abb5c5a20cfabfd70
RahulTIgerz <100237086+RahulTIgerz@users.noreply.github.com>
2022-05-08 15:03:06 +1200

    Update

--- internal_game_rework.py
+++ internal_game_rework.py
@@ -1,54 +1,74 @@
-
+ 
 #Imports
 from adventurelib import *
 Room.items = Bag()
 
 
 #Define Rooms
-science_class = Room("""
-
-\nLook for a door key.
+science_class = Room("""	
+The first room, The science class.
+The room is very dark and gloomy however 
+You see there is a Black door.
+\nLook for a door key and use it,\n then open to see where it takes you.
 """)
 
 hallway_1 = Room("""
-
-\n You should look for a key or some sort 
+The mysterious hallway with Blinkering Lights.
+There is an unlocked door stained with blood up ahead.  
+\n You should look for a key or some sort,\n it may be useful to you soon. 
 """)
 
 english_class = Room("""
-  
+The detention room, The english class.
+The room is creepily quiet that it gives you chills up your spine.
+There is a partially covered secret passage that requires a key.
+\n Do you have a book key on you,\n I heard it was in the hallway.\n you should probably look for another key.   
 """)
 
 secret_passage = Room("""
-
+The spy vent, The secret passage.
+Over here you can see everything underneath, You see a girl laughing crazily.
+You then instantly get the feeling shes bad news and you need to escape.
+There is a vent maybe that leads to the way out ?
+\n Did you get a vent key in english class, \n If so use it on the vent.
 """)
 
 science_class_midgame = Room("""
-
-\nLook for a door key.
+The first room, The science class.
+Your back to the dark gloomy room 
+however you notice an unlocked door on your west.
+\n Check if there is anything here, then head on out. \n Be careful you do not know where she is or what she has planned
 """)
 
 hallway_2 = Room("""
-
+"BEEP"
+Shoot you here the alarm go off, quick
+There is a gap underneath the stairs up ahead 
+\nGo east towards the stairs and hide.
 """)
 
 start_of_stairs = Room("""
-
+She arrives into the hallway and does not see you, 
+She then goes up the stairs armed with a knife.
+You feel something poking you on your back. 
+\n Look for what it is,\n and maybe it could help you defeat her.\n Go upstairs (North) to face her. 
 """) 
 
 boss_room = Room("""
 
 """)
 
-#Define Connections - Add some more
+#Define Connections 
 science_class.east = hallway_1
 hallway_1.east = english_class
 english_class.south = secret_passage
 secret_passage.south = science_class_midgame 
-science_class_midgame.west = start_of_stairs
+science_class_midgame.west = hallway_2
+hallway_2.east = start_of_stairs	
 start_of_stairs.north = boss_room
 
-#Define Items
+#Define Items 
+#@Make it that there are multiple options the user can call out. Also make sure u add some more description. 
 Item.description = "" #This adds a blank description to each item
 
 door_key = Item("door key", "door")
@@ -65,8 +85,8 @@ katana.description = ("An authentic japanese katana, when unsheaved it is very s
 
 #Define Bags - Do it in order
 science_class.items.add(door_key)
-hallway_1.items.add(vent_key)
-english_class.items.add(book_key)
+hallway_1.items.add(book_key)
+english_class.items.add(vent_key)
 boss_room.items.add(katana)
 
 
@@ -80,7 +100,8 @@ inventory = Bag()
 door_opened = False
 
 #Binds (eg "@when(look"))
-
+#@Add/ Addapt your binds inlcuding some loops.
+#@Create a hide function which is a while loop that says Come on hide everytime till they say hide.
 
 @when("look")
 def look():
@@ -162,10 +183,10 @@ def look_at(item):
 	else:
 		print(f"You aren't carrying an {item}")
 
-@when ("go DIRECTION") #Work on
+@when ("go DIRECTION") 
 def travel(direction):
 	global current_room
-	if door_opened == False:
+	if door_opened == True:
 		current_room = current_room.exit(direction)
 		print(f"You go {direction}.")
 		print(current_room)
